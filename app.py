ebc728d1083f0d02eb9c1a211e978e9c8f48220d
Eric Einspänner <60260090+Ede1994@users.noreply.github.com>
2025-02-07 22:29:02 +0100

    add Mistral AI and update gemini api

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ __version__ = "0.1.0"
 import os
 from pathlib import Path
 import streamlit as st
-import google.generativeai as genai
+# import google.generativeai as genai
+from google import genai
+from mistralai import Mistral
 from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
 from utils.utils import get_video_transcript, load_prompt
@@ -28,14 +30,62 @@ from utils.utils import get_video_transcript, load_prompt
 # ***************************************************************************
 # Load environment variables
 load_dotenv()
+
+### Google Gemini
 # Get API keys from environment variables
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-# Initializing model
-model = genai.GenerativeModel(model_name="gemini-1.5-pro")
+def gemini_chat(content, api_key=GEMINI_API_KEY, model="gemini-2.0-flash"):
+    r"""
+    Chat with the Gemini API.
+    
+    Args:
+        api_key (str): The API key for the Gemini API.
+        model (str): The model to use for the chat.
+        content (str): The content to send to the API.
+    
+    Returns:
+        str: The response from the API.
+    """
+    # Initialize the Gemini API
+    client = genai.Client(api_key=api_key)
+    
+    # Chat with the API
+    response = client.models.generate_content(model=model,
+                                              contents=content)
+    
+    # Return the response
+    return response.text
 
-# Configure the API Key
-genai.configure(api_key=GEMINI_API_KEY)
+
+### Mistral
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
+def mistral_chat(content, api_key=MISTRAL_API_KEY, model="mistral-small-latest"):
+    r"""
+    Chat with the Mistral API.
+    
+    Args:
+        api_key (str): The API key for the Mistral API.
+        model (str): The model to use for the chat.
+        messages (list): A list of messages to send to the API.
+    
+    Returns:
+        str: The response from the API.
+    """
+    # Initialize the Mistral API
+    mistral = Mistral(api_key=api_key)
+    
+    # Chat with the API
+    response = mistral.chat.complete(model=model, messages=[
+        {
+            "content": content,
+            "role": "user",
+        },
+    ], stream=False)
+
+    # Return the response
+    return response.choices[0].message.content
 
 
 # ***************************************************************************
@@ -113,12 +163,13 @@ def main():
                 prompt = summarize_prompt.format(transcript=transcript)
                 
                 # Generate summary
-                response = model.generate_content(prompt)
+                response = gemini_chat(content=prompt)
+                # response = mistral_chat(content=prompt)
                 
                 # Display summary in a styled box
                 st.markdown('<div class="summary-box">', unsafe_allow_html=True)
                 st.subheader("📊 Video Summary")
-                st.write(response.text)
+                st.write(response)
                 st.markdown('</div>', unsafe_allow_html=True)
 
     # Notes button
@@ -137,12 +188,13 @@ def main():
                 prompt = notes_prompt.format(transcript=transcript)
                 
                 # Generate notes
-                response = model.generate_content(prompt)
+                response = gemini_chat(content=prompt)
+                # response = mistral_chat(content=prompt)
                 
                 # Display notes in a styled box
                 st.markdown('<div class="notes-box">', unsafe_allow_html=True)
                 st.subheader("📊 Video Notes")
-                st.write(response.text)
+                st.write(response)
                 st.markdown('</div>', unsafe_allow_html=True)
 
     # Close the button-container div
